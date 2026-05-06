a98c912cde2f1a215c4fb36d1f31f8c6f3dc2f78
Kaveesha Nethmi Shehani <kaveeshanethmi0705@gmail.com>
2026-05-06 13:03:47 +0530

    uplode CustomerModel.js

--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -3,3 +3,10 @@ import {customers} from '../db/db.js';
 export function saveCustomer(customer) {
     customers.push(customer);
 }
+
+export function updateCustomer(updatedCustomer) {
+    const index = customers.findIndex(c => c.id === updatedCustomer.id);
+    if (index !== -1) {
+        customers[index] = updatedCustomer;
+    }
+}
