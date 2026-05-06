d6d396d0331ebb8ba4bfffec8a28c772464dc43e
Kaveesha Nethmi Shehani <kaveeshanethmi0705@gmail.com>
2026-05-06 12:33:48 +0530

    customerController.js uplode

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -4,3 +4,13 @@ import { saveCustomer, updateCustomer, deleteCustomer, getAllCustomers } from '.
 
 let nextId = 1;
 let selectedCustomerId = null;
+
+// ---- Helper: Show Alert ----
+function showAlert(message, type = 'success') {
+    Swal.fire({
+        icon: type === 'danger' ? 'error' : type,
+        title: message,
+        showConfirmButton: false,
+        timer: 1500
+    });
+}
